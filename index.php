c939a5f2856ffb296caf0013c9f4b10871daf9e8
bonaganda <bonalizagarcia@gmail.com>
2017-05-30 12:46:56 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+<title>User Profile</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="CSS/styleSheet.css"/>
+    
+    <body style="background: url(Images/websitebackground.jpg)">
+        <?php include 'Navbar.php';?><br>
+        <?php include './HomeGames.php';?><br>
+    
+        <!-- Page Container -->
+        <div class="w3-content w3-margin-top" style="max-width:800px;">
+        <!-- End Page Container -->
+        
+        </div>
+        <br><br><br><br><br><br> <br>
+        <footer class="w3-container w3-jd w3-center w3-margin-top">
+            <p><font color = "white" font = "Arial"><b>Follow Us</b></font></p>
+            <a href="https://www.facebook.com/theinstillery/"><i class="fa fa-facebook-official w3-xlarge w3-hover-opacity w3-white"></i></a>
+            <a href="https://twitter.com/theinstillery?lang=en"><i class="fa fa-twitter w3-xlarge w3-hover-opacity w3-white"></i></a>
+            <a href="https://www.linkedin.com/company/the-instillery"><i class="fa fa-linkedin w3-xlarge w3-hover-opacity w3-white"></i></a>
+        </footer>
+
+    </body>
+</html>
