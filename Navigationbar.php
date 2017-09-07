9980a5a723a7fc10441446996006706d1edc1604
John Barraca <tolmike23@gmail.com>
2017-09-08 02:47:32 +1200

    HomePage,Login,Register,Profile
    Hi Team
    
    Homepage, Login, Profile, Register and Leaderboards are fully working I made some changes on Register where it can add uniqueID to both gameData and Register  tables.

--- Navigationbar.php
+++ Navigationbar.php
@@ -1,39 +1,19 @@
 
-<link rel="stylesheet" type="text/css" href="CSS/navBar.css"/><nav class="navigation">
+<link rel="stylesheet" type="text/css" href="CSS/navBar.css"/>
+<link href="http://cdn.phpoll.com/css/animate.css" rel="stylesheet">
+<nav class="navigation">
     <ul id="nav">
-        <a href="index.php"><img src="Images/inst.png" width="232" height="120" id="logo" /></a> 
+        <a href="index.html"><img src="Images/inst.png" width="232" height="120" id="logo" /></a>
         <li><a href="">TOURNAMENTS</a></li>
-        <li class="dropdown" style="position: relative; top:">
-            <div><a href="javascript:void(0)" class="dropbtn" onclick="myFunction()">RANKINGS</a></div>
-            <div class="dropdown-content" id="myDropdown">
-                <a href="">WINS</a>
-                <a href="">WIN-RATE</a>
-                <a href="">TROPHIES</a>
-            </div>
-        </li> 
+        <li><a href="">RANKINGS</a></li>
         <li><a href="UserProfile.php">PROFILES</a></li>
         <li><a href="leaderboard.php">LEADERBOARD</a></li>
-        <li><a href="RegisterForm.php">REGISTER</a></li>
-        <?php if (isset($_SESSION['username'])) { ?>
-            <li class="dropdown" style="position: relative; top:">
-                <a href="javascript:void(0)" class="dropbtn" onclick="myFunction()"><?php echo $_SESSION['username']; ?></a>
-                <div class="dropdown-content">
-                    <a href="#">My Favorites</a>
-                    <div class="dropdown-content-sub">
-                        <?php while ($counter != -1) { ?>
-                            <a href="./Games/<?php echo $fave[$counter] ?>.php"><?php echo $fave[$counter] ?></a>
-                            <?php
-                            $counter--;
-                        }
-                        ?>`
-                    </div>
-                    <a href="Logout.php">Logout</a>
-                </div>
-            </li>
-        <?php } else { ?>
-            <li><a href="Login.html">LOGIN</a></li>
-        <?php } ?>
-
+        <li><a href="RegisterForm.php"></a></li>
+        <li><a href="logout.php" id="logout"></a></li>
     </ul>
 </nav>
+
+<!--logout';-->
+<script src="js/jquery.js"></script>
+<script src="js/user.js"></script>
 
